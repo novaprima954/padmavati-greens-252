095c135310448f6aad3bf75e15593398b088a19d
novaprima954 <advaitpaltewar@gmail.com>
2026-02-24 17:03:45 +0530

    app folder

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
 // ============================================================
 
 const CONFIG = {
-  API_URL:      'https://script.google.com/macros/s/AKfycby-NBAs_wufuPXhU0cLEokkyWmMIYA3OLj6D_mA2UEYJdHAR2a45QxzlWDaXG6aEZFK/exec', // ← Paste your deployed Apps Script URL here
+  API_URL:      'https://script.google.com/macros/s/AKfycbyPOo8YCLFSnQvDugHs1fkQwOKNYGi8fWx1kKrpw3k12k57mGS6Fm8vfMyUQ0KJni_F/exec', // ← Paste your deployed Apps Script URL here
   PROJECT_NAME: 'Padmavati Greens',
   LAYOUT:       'Layout No. 712 · Survey No. 274 · Yavatmal',
   RECEIPT_PREFIX: 'PG'
